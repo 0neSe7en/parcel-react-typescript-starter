b8352b076b35526e4f59333d3eb21a0c1ba6f0f1
0neSe7en <siyuan.se7en@gmail.com>
2021-10-27 12:00:25 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <div className="flex items-center justify-center bg-red-50">
+      <h1 className="text-center text-xl font-bold bg-red-100 w-[300px] h-[200px]">Hello World</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode: "jit",
+    purge: [
+        './src/**/*.html',
+        './src/**/*.js',
+        './src/**/*.ts',
+        './src/**/*.tsx',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
